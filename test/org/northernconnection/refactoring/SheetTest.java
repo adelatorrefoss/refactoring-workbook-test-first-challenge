e3832d586bda82072c320cab30da55fcb7b491e1
Antonio de la Torre <antonio@codesai.com>
2019-02-05 12:17:50 +0100

    REFACTOR: rename file

--- test/org/northernconnection/refactoring/SheetTest.java
+++ test/org/northernconnection/refactoring/SheetTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class SimpleSpreadsheetTest {
+public class SheetTest {
 
     @Test
     public void testThatCellsAreEmptyByDefault() {
