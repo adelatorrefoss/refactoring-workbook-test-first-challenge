5e2630aefb6e2c70d59887a9ac32d7d276722247
Antonio de la Torre <antonio@codesai.com>
2019-02-05 12:31:02 +0100

    GREEN: testThatManyCellsExist. I realized that the previous step didn't small enough. I could have done an unique cell system.

--- test/org/northernconnection/refactoring/SheetTest.java
+++ test/org/northernconnection/refactoring/SheetTest.java
@@ -29,25 +29,24 @@ public class SheetTest {
         assertEquals("", sheet.get(theCell));
     }
 
-//// Implement each test before going to the next one; then refactor.
-//
-//    public void testThatManyCellsExist() {
-//        Sheet sheet = new Sheet();
-//        sheet.put("A1", "First");
-//        sheet.put("X27", "Second");
-//        sheet.put("ZX901", "Third");
-//
-//        assertEquals("A1", "First", sheet.get("A1"));
-//        assertEquals("X27", "Second", sheet.get("X27"));
-//        assertEquals("ZX901", "Third", sheet.get("ZX901"));
-//
-//        sheet.put("A1", "Fourth");
-//        assertEquals("A1 after", "Fourth", sheet.get("A1"));
-//        assertEquals("X27 same", "Second", sheet.get("X27"));
-//        assertEquals("ZX901 same", "Third", sheet.get("ZX901"));
-//    }
-//
-//
+    @Test
+    public void testThatManyCellsExist() {
+        Sheet sheet = new Sheet();
+        sheet.put("A1", "First");
+        sheet.put("X27", "Second");
+        sheet.put("ZX901", "Third");
+
+        assertEquals("A1", "First", sheet.get("A1"));
+        assertEquals("X27", "Second", sheet.get("X27"));
+        assertEquals("ZX901", "Third", sheet.get("ZX901"));
+
+        sheet.put("A1", "Fourth");
+        assertEquals("A1 after", "Fourth", sheet.get("A1"));
+        assertEquals("X27 same", "Second", sheet.get("X27"));
+        assertEquals("ZX901 same", "Third", sheet.get("ZX901"));
+    }
+
+
 //// Implement each test before going to the next one.
 //// You can split this test case if it helps.
 //
